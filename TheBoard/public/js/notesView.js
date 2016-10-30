feaf9b32bd2d2d4ec35483550473070167e9ae68
Leonardo de Lima Cruz <leonardo.cruz@outlook.com>
2016-10-30 20:57:08 -0200

    Commiting project

---
+++ TheBoard/public/js/notesView.js
@@ -0,0 +1,43 @@
+﻿(function (angular) {
+    
+    var theModule = angular.module("notesView", ["ui.bootstrap"]);
+    
+    theModule.controller("notesViewController",
+        ["$scope", "$window", "$http",
+        function ($scope, $window, $http) {
+            $scope.notes = [];
+            $scope.newNote = {
+                note: "",
+                color: "yellow"
+            };
+            
+            //get the notes
+            var urlParts = $window.location.pathname.split("/");
+            var categoryName = urlParts[urlParts.length - 1];
+            
+            var notesUrl = "/api/notes/" + categoryName;
+            $http.get(notesUrl)
+                .then(function (result) {
+                    $scope.notes = result.data;
+                }, function (err) {
+                    alert(err);
+            });
+
+            $scope.save = function () {
+
+                $http.post(notesUrl, $scope.newNote)
+                .then(function (result) {
+                    $scope.notes.push(result.data);
+                    $scope.newNote = {
+                        note: "",
+                        color: "yellow"
+                    };
+                }, function (err) {
+                    alert(err);
+                });
+
+            };
+        }
+    ]);
+
+})(window.angular);
---
+++ TheBoard/server.js
@@ -0,0 +1,43 @@
+﻿var http = require("http");
+var express = require("express");
+var session = require("express-session");
+var app = express();
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var controllers = require("./controllers");
+var flash = require("connect-flash");
+//var ejsEngine = require("ejs-locals");
+
+//Setup the View Engine
+//app.set("view engine", "jade");
+//app.engine("ejs", ejsEngine); //support master pages
+//app.set("view engine", "ejs"); //ejs view engine
+app.set("view engine", "vash"); //vash view engine
+
+//opt into services
+var parseUrlencoded = require('urlencoded-request-parser');
+var opts = { arrayLimit: 0 };
+app.use(parseUrlencoded(opts));
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(session({ secret: "PluralsightTheBoard" }));
+app.use(flash());
+
+
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
+// map the route
+controllers.init(app);
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Leo", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer(app);
+
+
+server.listen(3000);
